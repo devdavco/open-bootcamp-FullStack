96e56d1ff31f4a1722c54ab028a3b20df79ca20d
dacoa <98057946+dacoa@users.noreply.github.com>
2022-07-20 19:28:55 -0500

    Update ejercicio4_2.py

--- Python OpenBootcamp/ejercicio4_2.py
+++ Python OpenBootcamp/ejercicio4_2.py
@@ -8,15 +8,17 @@ numero_final = input("Ingrese número de final: ")
 numeros_impares = []
 
 while True:
-    if (numero_inicial.isnumeric() and numero_inicial.isnumeric()) and (int(numero_inicial) < int(numero_final)):
-        for numero in range(int(numero_inicial),int(numero_final)+1):
-            if numero % 2 != 0:
-                numeros_impares.append(numero)
-        break
+    if (numero_inicial.isnumeric() and numero_inicial.isnumeric()):
+        if (int(numero_inicial) >= int(numero_final)):
+            numero_final = input("El número final debe ser mayor que el primer número. Introduce otro: ")
+        else:
+            for numero in range(int(numero_inicial),int(numero_final)+1):
+                if numero % 2 != 0:
+                    numeros_impares.append(numero)
+            break
     else:
-        print("Ingrese los números correctamente")
+        print("Ingrese solo números")
         numero_inicial = input("Ingrese número de inicio: ")
         numero_final = input("Ingrese número de final: ")
-
 
 print(numeros_impares)
