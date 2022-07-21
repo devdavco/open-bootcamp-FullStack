d945b2136471e1b68754dd903d909fe8f6b383c2
dacoa <98057946+dacoa@users.noreply.github.com>
2022-07-20 19:43:45 -0500

    Update ejercicio4_3.py

--- Python OpenBootcamp/ejercicio4_3.py
+++ Python OpenBootcamp/ejercicio4_3.py
@@ -5,3 +5,11 @@ for numero in range (1,101):
     numeros.append(numero)
 
 print(sorted(numeros,reverse=True))
+
+
+print("-----Other way-----")
+
+# Escribe un programa que sea capaz de mostrar los números del 1 al 100 en orden inverso.
+numeros = range (1,101)
+
+print(sorted(numeros,reverse=True))
