fb512bf2aab7215705fa8f965bc71a23dadfb8a0
dacoa <98057946+dacoa@users.noreply.github.com>
2022-07-24 18:20:04 -0500

    Add files via upload

--- Python OpenBootcamp/ejercicio6_1.py
+++ Python OpenBootcamp/ejercicio6_1.py
@@ -1,3 +1,19 @@
+# En este ejercicio vais a crear la clase Vehículo la cual tendrá los siguientes atributos:
+
+# Color
+
+# Ruedas
+
+# Puertas
+
+# Por otro lado crearéis la clase Coche la cual heredará de Vehículo y tendrá los siguientes atributos:
+
+# Velocidad
+
+# Cilindrada
+
+# Por último, tendrás que crear un objeto de la clase Coche y mostrarlo por consola.
+
 class Vehiculo():
 
     def __init__(self,color,ruedas,puertas):
