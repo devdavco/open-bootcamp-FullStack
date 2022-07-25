0e698c9120775ae878475be12308383e596c0c14
dacoa <98057946+dacoa@users.noreply.github.com>
2022-07-25 08:26:49 -0500

    Add files via upload

---
+++ Python OpenBootcamp/ejercicio7.1/main.py
@@ -0,0 +1,14 @@
+import operaciones as op
+
+def main():
+    suma = op.sumar(2,4)
+    print(suma)
+    resta = op.restar(6,7)
+    print(resta)
+    multiplica = op.multiplicar(3,4)
+    print(multiplica)
+    divide = op.dividir(123,5)
+    print(divide)
+    
+if __name__ == '__main__':
+    main()
