385749b0a0d7b4fecafce9d741e6d852c239b103
dacoa <98057946+dacoa@users.noreply.github.com>
2022-07-25 08:32:03 -0500

    Update main.py

--- Python OpenBootcamp/ejercicio7.1/main.py
+++ Python OpenBootcamp/ejercicio7.1/main.py
@@ -1,14 +1,10 @@
 import operaciones as op
 
 def main():
-    suma = op.sumar(2,4)
-    print(suma)
-    resta = op.restar(6,7)
-    print(resta)
-    multiplica = op.multiplicar(3,4)
-    print(multiplica)
-    divide = op.dividir(123,5)
-    print(divide)
+    print(sumar(2,4))
+    print(restar(6,7))
+    print(multiplicar(3,4))
+    print(dividir(123,5))
     
 if __name__ == '__main__':
     main()
