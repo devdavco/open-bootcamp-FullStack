08eccb9f18cee72a99516cbad11a99064db609c7
dacoa <98057946+dacoa@users.noreply.github.com>
2022-07-25 10:39:07 -0500

    Update ejercicio7_2.py

--- Python OpenBootcamp/ejercicio7_2.py
+++ Python OpenBootcamp/ejercicio7_2.py
@@ -1,11 +1,12 @@
 import time 
 def main():
     hora = time.localtime().tm_hour
+    minutos = time.localtime().tm_min
 
-    if hora > 19:
+    if hora >= 19:
         print("Es hora de ir a casa")
     else:
-        print("Te quedan:", 19 - hora," horas de trabajo")
+        print(f"Te quedan: {18-hora} horas y {59-minutos} minutos de trabajo")
 
 if __name__ == '__main__':
     main()
