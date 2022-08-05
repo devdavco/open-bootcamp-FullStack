8120d7bfc7a8c3d8330ad89af0fc89224745a40a
David Corrales <98057946+devdavco@users.noreply.github.com>
2022-08-05 09:13:27 -0500

    Update ejercicio9_1.py

--- Python OpenBootcamp/ejercicio9_1.py
+++ Python OpenBootcamp/ejercicio9_1.py
@@ -2,5 +2,7 @@
 paises = 'Colombia, Holanda, Argentina, Italia, Venezuela, Japón, Argentina, Alemania, suizA, nuEva Zelanda, España, COLombia'
 paises_guardados = sorted(set(paises.replace(', ',',').title().split(',')),key=str.lower)
 
-for pais in paises_guardados:
-    print(pais+ ", ",end='')
+print(', '.join(paises_guardados))
+
+# for pais in paises_guardados:
+#    print(pais+ ", ",end='')
