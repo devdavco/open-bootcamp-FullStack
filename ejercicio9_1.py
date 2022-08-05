d3bf6f01bd1b73a51b5406b577901f3cb3ddbfcf
David Corrales <98057946+devdavco@users.noreply.github.com>
2022-08-05 09:03:38 -0500

    Delete ejercicio9_1.py

--- ejercicio9_1.py
+++
@@ -1,8 +0,0 @@
-
-
-paises = str(input('Ingrese nombre de paises separados por comas (,) '))
-# paises = 'Colombia, Holanda, Argentina, Italia, Venezuela, Japón, Argentina, Alemania, suizA, nuEva Zelanda, España, COLombia'
-paises_guardados = sorted(set(paises.replace(', ',',').title().split(',')),key=str.lower)
-
-for pais in paises_guardados:
-    print(pais+ ", ",end='')
