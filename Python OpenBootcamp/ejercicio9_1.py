8965c641096cdaeacaf88df17d04a2a5a5c4a46d
David Corrales <98057946+devdavco@users.noreply.github.com>
2022-08-05 09:04:06 -0500

    Update ejercicio9_1.py

--- Python OpenBootcamp/ejercicio9_1.py
+++ Python OpenBootcamp/ejercicio9_1.py
@@ -1,7 +1,5 @@
-
-
-paises = str(input('Ingrese nombre de paises separados por comas (,) '))
-# paises = 'Colombia, Holanda, Argentina, Italia, Venezuela, Japón, Argentina, Alemania, suizA, nuEva Zelanda, España, COLombia'
+# paises = str(input('Ingrese nombre de paises separados por comas (,) '))
+paises = 'Colombia, Holanda, Argentina, Italia, Venezuela, Japón, Argentina, Alemania, suizA, nuEva Zelanda, España, COLombia'
 paises_guardados = sorted(set(paises.replace(', ',',').title().split(',')),key=str.lower)
 
 for pais in paises_guardados:
