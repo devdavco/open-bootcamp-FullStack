16061df9ed4b53492046ccc80c7370ce3bb48189
David Corrales <98057946+devdavco@users.noreply.github.com>
2022-08-05 21:39:03 -0500

    Update ejercicio9_2.py

--- Python OpenBootcamp/ejercicio9_2.py
+++ Python OpenBootcamp/ejercicio9_2.py
@@ -2,18 +2,16 @@ from functools import reduce
 
 
 def num_impares(lista):
-    if lista %2 != 0:
+    if lista %2:
         return True
     return False
 
 lista = [1,2,3,4,5,6,7,8,9,10]
 
-
-resultado_filter = filter(num_impares ,lista)
-# print(list(resultado_filter))
+resultado_filter = list(filter(num_impares ,lista))
+print(resultado_filter)
 
 resultado_reduce = reduce(lambda a,b:a+b,resultado_filter)
-
-print(list(resultado_filter))
 print(resultado_reduce)
+
 
